491482c8217327b86d46e01d143c333377bc8687
zhijun_wang <zhijun_wang@kingdee.com>
2014-11-28 17:14:13 +0800

    height bug
    height bug

--- demo/android-app/mobile-login/javascript/main.js
+++ demo/android-app/mobile-login/javascript/main.js
@@ -7,8 +7,10 @@
 var username = document.getElementById("username");
 var password = document.getElementById("password");
 var info = document.getElementById("show-info");
+var footer = document.getElementById("footer");
 var name = "admin"
 var pass = "9595f446f7db1a9b2d19e8d45f2604f3";
+footer.style.top = (window.innerHeight - 30) + 'px';
 function logon(){
   	if(username.value == name && hex_md5(password.value) == pass){
        return true;
